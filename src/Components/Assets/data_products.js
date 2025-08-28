67ab68033f22e90ee0390211992b9d1e12702c3b
Kadir Yavuz <137656671+kadiryavuz1@users.noreply.github.com>
2025-08-28 19:45:53 +0300

    update

--- src/Components/Assets/data_products.js
+++ src/Components/Assets/data_products.js
@@ -25,7 +25,7 @@ const data_products = [
     },
     img: iptv_logo,
     duration: "3 Months",
-    price: 35,
+    price: 40,
     type: "premium"
   },
   {
@@ -52,7 +52,7 @@ const data_products = [
     },
     img: iptv_logo,
     duration: "6 Months",
-    price: 45,
+    price: 50,
     type: "premium"
   },
   {
@@ -79,7 +79,7 @@ const data_products = [
     },
     img: iptv_logo,
     duration: "12 Months",
-    price: 70,
+    price: 75,
     type: "premium"
   },
   {
@@ -106,7 +106,7 @@ const data_products = [
     },
     img: iptv_logo,
     duration: "15 Months",
-    price: 80,
+    price: 85,
     type: "premium"
   },
   {
@@ -133,7 +133,7 @@ const data_products = [
     },
     img: iptv_logo,
     duration: "24 Months",
-    price: 100,
+    price: 120,
     type: "premium"
   },
   
